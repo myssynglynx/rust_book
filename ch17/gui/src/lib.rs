c4178fb0d7c38d7f68d7145e27a889c991efd789
myssynglynx <myssynglynx@protonmail.com>
2022-08-22 00:11:57 -0700

    cleanup

---
+++ ch17/gui/src/lib.rs
@@ -0,0 +1,32 @@
+// Base Draw Trait
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+// Screen Object
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw()
+        }
+    }
+}
+
+// Button Object
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+    }
+}
